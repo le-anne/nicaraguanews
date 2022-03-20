b739d277b46113342f1f43f8a221e03ed9b78849
Leanne <76232496+le-anne@users.noreply.github.com>
2022-03-20 15:52:12 -0700

    built header component

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>Nicaragua News en English</h1>
+      <div className="Title-Subtitle">
+        Nicaragua news from sources all over the world.
+      </div>
+    </div>
+  );
+}
+
+export default Title;
---
+++ src/news.js
@@ -0,0 +1,14 @@
+fetch("https://nicaragua-news-en-english.p.rapidapi.com/news", {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "nicaragua-news-en-english.p.rapidapi.com",
+    "x-rapidapi-key": "ba24453ed6mshf63b94eccba3171p1c4965jsnf3c1b12fbe39",
+  },
+})
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
