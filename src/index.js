7ffede0d48da697af50c75ff4f2b59626174e668
Leanne <76232496+le-anne@users.noreply.github.com>
2022-06-17 18:18:06 -0700

    added header images and responsive

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./App.css";
+import Header from "./NicaraguaHeader4.png";
 import Title from "./components/Title";
 import Body from "./components/Body";
 
 ReactDOM.render(
   <React.StrictMode>
+    <img src={Header} className="nicaHeader" alt="Nicarauga News" />
     <Title />
     <Body />
   </React.StrictMode>,
